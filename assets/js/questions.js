1e3aa2439321ee77cf9b89ed734b1e2b1aa1b49c
ayanfetech <111120851+ayanfetech@users.noreply.github.com>
2022-10-08 14:11:10 +0100

    Add files via upload

---
+++ assets/js/questions.js
@@ -0,0 +1,262 @@
+let questions = [
+  {
+    numb: 1,
+    question: "A collection of records is_______?",
+    answer: "File",
+    options: ["Function", "File", "File Upload", "Date"]
+  },
+  {
+    numb: 2,
+    question: "Brushing teeth is twice a day?",
+    answer: "True",
+    options: ["True", "False", "Sometimes", "everytime"]
+  },
+  {
+    numb: 3,
+    question: "All are input device except?",
+    answer: "Monitor",
+    options: ["Mouse", "Monitor", "Light Pen", "Keyboard"]
+  },
+  {
+    numb: 4,
+    question: "A place where buyers and sellers meet is called?",
+    answer: "Market",
+    options: ["Shop", "Office", "Market", "Road"]
+  },
+  {
+    numb: 5,
+    question: "The person who produce drug is_______",
+    answer: "Pharmacist",
+    options: ["Pharmacy", "Pharmacism", "Parmacist", "Pharmacist"]
+  },
+
+  {
+    numb: 6,
+    question: "A place where two rivers meet is called_____?",
+    answer: "Confluence",
+    options: ["Valley", "Lake", "Confeluence", "Confluence"]
+  },
+
+  {
+    numb: 7,
+    question: "What does CDN stand for?",
+    answer: "Content Delivery Network",
+    options: [
+      "Content Delivery Network",
+      "Create Delivery Network",
+      "Cascading Development Network",
+      "Content Delivery Natation"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "What does SEO stand for?",
+    answer: "Search Engine Optimization",
+    options: [
+      "Stylish Engine Object",
+      "Search Engine Optimization",
+      "Statement Engine Optimization",
+      "Searh Environment Object"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "What does CSS stands for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Cascading Styling Sheet",
+      "Cascade Style Sheet",
+      "Cascadent Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "What is the largest search engine?",
+    answer: "Google",
+    options: ["Yahoo", "Bing", "Facebook", "Google"]
+  },
+
+  {
+    numb: 11,
+    question: "What language is a markup language?",
+    answer: "HTML",
+    options: ["JAVA", "PYTHON", "GOLANG", "HTML"]
+  },
+
+  {
+    numb: 12,
+    question: "What does INEC stands for?",
+    answer: "Independent National Electoral Commission",
+    options: [
+      "Independent Nigeria Electoral Commission",
+      "International National Electoral Commission",
+      "Independent National Election Commission",
+      "Independent National Electoral Commission"
+    ]
+  },
+
+  {
+    numb: 13,
+    question: "First secondary school in Nigeria is_________?",
+    answer: "CMS Grammar School",
+    options: [
+      "CMS Grammar School",
+      "Solid Rock Academy",
+      "Remo Secondary School",
+      "Sagamu High School"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "What does ITF stand for?",
+    answer: "Industrial Training Fund",
+    options: [
+      "Independent Training Fund",
+      "Industrial Teaching Foundation",
+      "Industrial Training Fund",
+      "Institute Teaching Fund"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "What does WHO stand for?",
+    answer: "World Health Organization",
+    options: [
+      "World Health Organization",
+      "World Health Origination",
+      "World Heart Organization",
+      "Word Health Organization"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "Which country host FIFA 2022 CUP?",
+    answer: "Qatar",
+    options: ["Nigeria", "Japan", "Ghana", "Qatar"]
+  },
+
+  {
+    numb: 17,
+    question: "What does NECO stands for?",
+    answer: "National Examination Council",
+    options: [
+      "Nigeria Examination Concil",
+      "NAtional Exercise Country",
+      "National Examination Council",
+      "National Experiment Council"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "A person that attends to guest in an office is_____?",
+    answer: "Receptionist",
+    options: ["Manager", "Director", "Receptionist", "Reception"]
+  },
+
+  {
+    numb: 19,
+    question: "Who created the Internet?",
+    answer: "Tim Berners Lee",
+    options: ["Bill Gates", "Prof. Ahmed Musa", "Jeff Bezos", "Tim Berners Lee"]
+  },
+
+  {
+    numb: 20,
+    question: "A thought or suggestion is known as _______?",
+    answer: "Idea",
+    options: ["Thinking", "Idea", "Reason", "No idea"]
+  },
+
+  {
+    numb: 21,
+    question: "Who is the owner of facebook?",
+    answer: "Mark Zuckerberg",
+    options: ["Bill Gates", "Elon Musk", "Mark Zuckerberg", "Jeff Bezos"]
+  },
+
+  {
+    numb: 22,
+    question: "Facebook was change from its former name to______?",
+    answer: "Meta",
+    options: ["Whatsapp", "Meta", "TweetMe", "Metar"]
+  },
+
+  {
+    numb: 23,
+    question: "What does ND stands for?",
+    answer: "National Diploma",
+    options: [
+      "Nigeria Directorate",
+      "National Diplomatic",
+      "New Direction",
+      "National Diploma"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "What does PGD stands for?",
+    answer: "Post Graduate Diploma",
+    options: [
+      "Pre Graduate Diplom",
+      "Post Graduate Diplomee",
+      "Post Graduate Diploma",
+      "Per Grade Direction"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "What does CLI stands for?",
+    answer: "Command LIne Interface",
+    options: [
+      "Command Line Info",
+      "Center Line Info",
+      "Command LIne Interface",
+      "Correct Linear Interface"
+    ]
+  },
+
+  {
+    numb: 26,
+    question: "Best linux for pentesting is ____?",
+    answer: "Kali",
+    options: ["Windows", "Ubuntu", "Kalii", "Kali"]
+  },
+
+  {
+    numb: 27,
+    question: "Step by step procedure of solving task is ____?",
+    answer: "Algorithm",
+    options: ["Alghorithm", "Algorithm", "Algorithn", "Arlgorithm"]
+  },
+
+  {
+    numb: 28,
+    question: "All are examples of output device except_____?",
+    answer: "Mouse",
+    options: ["Monitor", "Speaker", "Printer", "Mouse"]
+  },
+
+  {
+    numb: 29,
+    question: "None is vector quantites except ____?",
+    answer: "Distance",
+    options: ["Height", "Weight", "Distance", "Width"]
+  },
+
+  {
+    numb: 30,
+    question: "All are figure of speech except____?",
+    answer: "Preposition",
+    options: ["Simile", "Preposition", "Metaphor", "Hyperbole"]
+  }
+];
